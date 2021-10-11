73a79e427c745e65d8506310163178a4c34c50ff
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-10-11 10:43:45 +0530

    Push Singleton demo

---
+++ DesignPatterns/DesignPatterns/ExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DesignPatterns
+{
+   public class ExceptionHandler
+    {
+        private static ExceptionHandler _instance = null;
+
+        private static StreamWriter _streamWriter;
+
+        private static object _lock = new object();
+        private ExceptionHandler()
+        {
+            _streamWriter = new StreamWriter("Error.txt");
+        }
+
+        public static ExceptionHandler Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ExceptionHandler();
+                    }
+                    return _instance;
+                }
+                
+            }
+            
+        }
+
+        public void WriteLog(string message)
+        {
+            _streamWriter.WriteLine(message);
+            _streamWriter.Flush();
+        }
+
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num1 = 10;
+                int num2 = 0;
+                int result = num1 / num2;
+                Console.WriteLine(result);
+
+            }catch(Exception ex)
+            {
+                ExceptionHandler.Instance.WriteLog(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
